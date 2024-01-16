124c77db5dbb695aa89aea3d0754cb9d8735313a
jnyawach <nyawach41@gmail.com>
2024-01-15 18:13:03 -0800

    updated clone

--- models/user.py
+++ models/user.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""
-Module for the User class.
-"""
+"""This module creates a User class"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """
-    class User that handles users' information
-    """
+    """Class for managing user objects"""
+
     email = ""
     password = ""
     first_name = ""
     last_name = ""
+
